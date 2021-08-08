fb6277174069f54ac26c3018e6e562de6880ad44
黄城 <9078881+huangchengkeepfighting@user.noreply.gitee.com>
2021-08-08 20:17:39 +0800

    Road

---
+++ day18/src/cn/zju/leetcode/code2_1137.java
@@ -0,0 +1,48 @@
+package cn.zju.leetcode;
+
+public class code2_1137 {
+    public int tribonacci1(int n) {
+        if (n <= 2) {
+            return n == 0 ? 0 : 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
+    }
+
+    public int tribonacci2(int n) {
+        if (n <= 2) {
+            return n == 0 ? 0 : 1;
+        }
+        int[][] q = {{1, 1, 1}, {1, 0, 0}, {0, 1, 0}};
+        int[][] res = pow(q, n);
+        return res[0][2];
+    }
+
+    public int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+
+    public int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j] + a[i][2] * b[2][j];
+            }
+        }
+        return c;
+    }
+
+}
