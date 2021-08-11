44587ba97bb2e65d2e9b4e3ef5df8a9f0bedf6f4
huangcheng <weidian@weidiandeMacBook-Pro.local>
2021-08-11 14:24:38 +0800

    Road

---
+++ day18/src/cn/zju/leetcode/code5_446.java
@@ -0,0 +1,27 @@
+package cn.zju.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class code5_446 {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        Map<Long, Integer>[] f = new Map[n];
+        for (int i = 0; i < n; i++) {
+            f[i] = new HashMap<>();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                long d = 1L * nums[i] - nums[j];
+                int cnt = f[j].getOrDefault(d, 0);
+                ans += cnt;
+                f[i].put(d, f[i].getOrDefault(d, 0) + cnt + 1);
+            }
+        }
+        return ans;
+    }
+
+}
