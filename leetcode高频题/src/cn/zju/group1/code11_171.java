1ef92652b5b477c91c6cc8e0b584f18905eec624
黄城 <9078881+huangchengkeepfighting@user.noreply.gitee.com>
2021-08-15 23:57:04 +0800

    Road

---
+++ leetcode高频题/src/cn/zju/group1/code11_171.java
@@ -0,0 +1,12 @@
+package cn.zju.group1;
+
+public class code11_171 {
+    public int titleToNumber(String columnTitle) {
+        char[] str = columnTitle.toCharArray();
+        int ans = 0;
+        for (int i = 0; i < str.length; i++) {
+            ans = ans * 26 + (str[i] - 'A') + 1;
+        }
+        return ans;
+    }
+}
---
+++ leetcode高频题/src/cn/zju/group1/code16_202.java
@@ -0,0 +1,16 @@
+package cn.zju.group1;
+
+public class code16_202 {
+    // 快乐数一定会经过1 或 4，经过1的就是快乐数。
+    public boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            int sum = 0;
+            while (n != 0) {
+                sum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+}
