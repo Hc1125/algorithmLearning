0bea44663b33d263965b0cd4942bb8476b037ced
huangcheng <weidian@weidiandeMacBook-Pro.local>
2021-08-17 15:53:15 +0800

    add

--- day18/src/cn/zju/leetcode/code13_1840.java
+++ day18/src/cn/zju/leetcode/code13_1840.java
@@ -17,6 +17,7 @@ public class code13_1840 {
         int maxHeight = (restrictions[0][1] + restrictions[0][0] - 1) >> 1;
         for (int i = 1; i < len; i++) {
             restrictions[i][1] = Math.min(restrictions[i][1], restrictions[i - 1][1] + restrictions[i][0] - restrictions[i - 1][0]);
+            // 根据数学关系获得两个限制中的最高高度
             maxHeight = Math.max(maxHeight, (restrictions[i - 1][1] + restrictions[i][1] + restrictions[i][0] - restrictions[i - 1][0]) >> 1);
         }
         maxHeight = Math.max(maxHeight, restrictions[len - 1][1] + n - restrictions[len - 1][0]);
