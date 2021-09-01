576a788450b0046e13abcb49ec6473ed409621c4
huangcheng <weidian@weidiandeMacBook-Pro.local>
2021-09-01 15:02:51 +0800

    Road

---
+++ day19/src/cn/zju/leetcode/code9_1109.java
@@ -0,0 +1,18 @@
+package cn.zju.leetcode;
+
+public class code9_1109 {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] ans = new int[n];
+        for (int[] booking : bookings) {
+            ans[booking[0] - 1] += booking[2];
+            if (booking[1] < n) {
+                ans[booking[1]] -= booking[2];
+            }
+        }
+        for (int i = 1; i < ans.length; i++) {
+            ans[i] += ans[i - 1];
+        }
+        return ans;
+    }
+
+}
