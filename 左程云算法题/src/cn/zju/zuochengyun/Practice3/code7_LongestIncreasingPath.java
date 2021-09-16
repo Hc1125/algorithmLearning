f5f7623c8ac96b65a8f5c259b7944c62c9dade27
HuangCheng <459299509@qq.com>
2021-09-16 15:06:04 +0800

    Road

--- 左程云算法题/src/cn/zju/zuochengyun/Practice3/code7_LongestIncreasingPath.java
+++ 左程云算法题/src/cn/zju/zuochengyun/Practice3/code7_LongestIncreasingPath.java
@@ -9,49 +9,37 @@ package cn.zju.zuochengyun.Practice3;
  * 2 1 3
  * 从最中心的1出发，是可以走出12345的链，而且这是最长的递增链。所以返回长度5
  */
+// https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix/
 public class code7_LongestIncreasingPath {
-    public static int maxPath1(int[][] matrix) {
-        int ans = Integer.MIN_VALUE;
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[0].length; col++) {
-                ans = Math.max(ans, process(matrix, row, col));
+    public static int longestIncreasingPath1(int[][] matrix) {
+        int ans = 0;
+        int N = matrix.length;
+        int M = matrix[0].length;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                ans = Math.max(ans, process1(matrix, i, j));
             }
         }
         return ans;
     }
 
-    public static int process(int[][] matrix, int i, int j) {
-        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) {
-            return -1;
-        }
-        int next1 = 0;
-        int next2 = 0;
-        int next3 = 0;
-        int next4 = 0;
-        if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j]) {
-            next1 = process(matrix, i - 1, j);
-        }
-        if (i + 1 < matrix.length && matrix[i + 1][j] > matrix[i][j]) {
-            next2 = process(matrix, i + 1, j);
-        }
-        if (j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j]) {
-            next3 = process(matrix, i, j - 1);
-        }
-        if (j + 1 < matrix[0].length && matrix[i][j + 1] > matrix[i][j]) {
-            next4 = process(matrix, i, j + 1);
-        }
-        return 1 + Math.max(Math.max(next1, next2), Math.max(next3, next4));
+    // 从m[i][j]开始走，走出来的最长递增链，返回！
+    public static int process1(int[][] m, int i, int j) {
+        int up = i > 0 && m[i][j] < m[i - 1][j] ? process1(m, i - 1, j) : 0;
+        int down = i < (m.length - 1) && m[i][j] < m[i + 1][j] ? process1(m, i + 1, j) : 0;
+        int left = j > 0 && m[i][j] < m[i][j - 1] ? process1(m, i, j - 1) : 0;
+        int right = j < (m[0].length - 1) && m[i][j] < m[i][j + 1] ? process1(m, i, j + 1) : 0;
+        return Math.max(Math.max(up, down), Math.max(left, right)) + 1;
     }
 
-
-
-
-    public static int maxPath2(int[][] matrix) {
-        int ans = Integer.MIN_VALUE;
-        int[][] dp = new int[matrix.length][matrix[0].length];
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[0].length; col++) {
-                ans = Math.max(ans, process(matrix, row, col, dp));
+    public static int longestIncreasingPath2(int[][] matrix) {
+        int ans = 0;
+        int N = matrix.length;
+        int M = matrix[0].length;
+        int[][] dp = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                ans = Math.max(ans, process2(matrix, i, j, dp));
             }
         }
         return ans;
@@ -64,35 +52,18 @@ public class code7_LongestIncreasingPath {
      * dp[i][j] != 0 process(i,j)之前已经算过了
      *
      */
-    public static int process(int[][] matrix, int i, int j, int[][] dp) {
-        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) {
-            return -1;
-        }
+    // 从m[i][j]开始走，走出来的最长递增链，返回！
+    public static int process2(int[][] m, int i, int j, int[][] dp) {
         if (dp[i][j] != 0) {
             return dp[i][j];
         }
-        int next1 = 0;
-        int next2 = 0;
-        int next3 = 0;
-        int next4 = 0;
-        if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j]) {
-            next1 = process(matrix, i - 1, j, dp);
-        }
-        if (i + 1 < matrix.length && matrix[i + 1][j] > matrix[i][j]) {
-            next2 = process(matrix, i + 1, j, dp);
-        }
-        if (j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j]) {
-            next3 = process(matrix, i, j - 1, dp);
-        }
-        if (j + 1 < matrix[0].length && matrix[i][j + 1] > matrix[i][j]) {
-            next4 = process(matrix, i, j + 1, dp);
-        }
-        int ans = 1 + Math.max(Math.max(next1, next2), Math.max(next3, next4));
+        // (i,j)不越界
+        int up = i > 0 && m[i][j] < m[i - 1][j] ? process2(m, i - 1, j, dp) : 0;
+        int down = i < (m.length - 1) && m[i][j] < m[i + 1][j] ? process2(m, i + 1, j, dp) : 0;
+        int left = j > 0 && m[i][j] < m[i][j - 1] ? process2(m, i, j - 1, dp) : 0;
+        int right = j < (m[0].length - 1) && m[i][j] < m[i][j + 1] ? process2(m, i, j + 1, dp) : 0;
+        int ans = Math.max(Math.max(up, down), Math.max(left, right)) + 1;
         dp[i][j] = ans;
         return ans;
     }
-
-
-
-
 }
