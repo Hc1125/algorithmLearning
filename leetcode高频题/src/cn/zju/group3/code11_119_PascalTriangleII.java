6abce3eed56bd4aed7091f497fb0eb792598b790
HuangCheng <459299509@qq.com>
2021-10-01 22:50:23 +0800

    Road

--- leetcode高频题/src/cn/zju/group3/code11_119_PascalTriangleII.java
+++ leetcode高频题/src/cn/zju/group3/code11_119_PascalTriangleII.java
@@ -3,7 +3,7 @@ package cn.zju.group3;
 import java.util.ArrayList;
 import java.util.List;
 
-public class code11_119 {
+public class code11_119_PascalTriangleII {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i <= rowIndex; i++) {
